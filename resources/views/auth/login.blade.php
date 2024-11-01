cb85e949e7a7abb558b8e42e04d49ba8bb1509aa
Jonyl Dave Villanueva <87189532+legendma13@users.noreply.github.com>
2024-11-01 16:46:20 +0800

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,45 +1,47 @@
 @include('partials.__header')
   <main class="custombg min-vh-100 loginpagebg">
     @include('partials.__nav')
-    <div class="container d-flex justify-content-center align-items-center py-3">
+    <div class="container d-flex justify-content-center align-items-center py-3" style="height: 80vh">
         <div class="card shadow-lg logincardbg" style="width: 100vh">
             <div class="card-body">
                 <div class="row">
                     <div class="col-lg-6 d-flex align-items-center justify-content-center mb-3">
-                        <img src="{{ URL::asset('img/logo.png') }}" alt="" class="img-fluid">
+                        <img src="{{ URL::asset('img/logo.png') }}" alt="" class="img-fluid d-none d-sm-block">
                     </div>
                     <div class="col-lg-6">
                         <div class="text-center">
                             <h3 class="fw-bold">LOGIN</h3>
                             <p>Sign in to continue</p>
                         </div>
-                        <form id="loginform" class="was-validated">
-                            <div class="form-group mb-3">
-                                <label class="form-label fw-bold" for="email">EMAIL</label>
-                                <input type="email" name="email" class="form-control py-3" id="email" placeholder="Enter your email address" required>
-                            </div>
-                            <div class="form-group mb-4">
-                                <label class="form-label fw-bold" for="password">PASSWORD</label>
-                                <input type="password" name="password" class="form-control py-3" id="password" placeholder="Enter your password address" required>
-                            </div>
-                            
-                            {{-- <div class="row">
-                                <div class="col-5 text-center"><hr style="width: 100%"></div>
-                                <div class="col-2 text-center">OR</div>
-                                <div class="col-5 text-center"><hr style="width: 100%"></div>
-                            </div> --}}
-                            {{-- <div class="text-center fw-bold mb-3"><h5>Login using <a href="{{ url('codelogin') }}" style="text-decoration: none">Code</a></h5></div> --}}
-                            <div class="mb-3 text-center">
-                                <div class="form-check d-flex justify-content-center">
-                                    <input class="form-check-input me-2" type="checkbox" value="" checked id="terms-and-condition" required />
-                                    <label class="form-check-label fs-6" for="terms-and-condition">
-                                        I agree to the <a href="#" data-bs-toggle="modal" data-bs-target="#termsModal">Terms and Conditions</a>
-                                    </label>
+                        <form id="loginform" class="was-validated d-flex align-items-center">
+                            <div style="width: 100%">
+                                <div class="form-group mb-3">
+                                    <label class="form-label fw-bold" for="email">EMAIL</label>
+                                    <input type="email" name="email" class="form-control py-3" id="email" placeholder="Enter your email address" required>
                                 </div>
-                            </div>
-                            @csrf
-                            <div class="text-center">
-                                <button class="btn btn-primary px-5 py-2 fw-100 rounded rounded-0" type="submit">LOGIN</button>
+                                <div class="form-group mb-4">
+                                    <label class="form-label fw-bold" for="password">PASSWORD</label>
+                                    <input type="password" name="password" class="form-control py-3" id="password" placeholder="Enter your password address" required>
+                                </div>
+                                
+                                {{-- <div class="row">
+                                    <div class="col-5 text-center"><hr style="width: 100%"></div>
+                                    <div class="col-2 text-center">OR</div>
+                                    <div class="col-5 text-center"><hr style="width: 100%"></div>
+                                </div> --}}
+                                {{-- <div class="text-center fw-bold mb-3"><h5>Login using <a href="{{ url('codelogin') }}" style="text-decoration: none">Code</a></h5></div> --}}
+                                <div class="mb-3 text-center">
+                                    <div class="form-check d-flex justify-content-center">
+                                        <input class="form-check-input me-2" type="checkbox" value="" checked id="terms-and-condition" required />
+                                        <label class="form-check-label fs-6" for="terms-and-condition">
+                                            I agree to the <a href="#" data-bs-toggle="modal" data-bs-target="#termsModal">Terms and Conditions</a>
+                                        </label>
+                                    </div>
+                                </div>
+                                @csrf
+                                <div class="text-center">
+                                    <button class="btn btn-primary px-5 py-2 fw-100 rounded rounded-0" type="submit">LOGIN</button>
+                                </div>
                             </div>
                         </form>
                     </div>
